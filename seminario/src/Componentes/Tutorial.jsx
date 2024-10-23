0b39ef3ea0c88dbf19e721c1404e0310815203c6
gonza <gonzalo.ruager@gmail.com>
2024-10-22 21:07:19 -0300

    Actualizacion de la estructura de texto del tutorial para poder resaltar palabras. Cambio de TabName e icono

--- seminario/src/Componentes/Tutorial.jsx
+++ seminario/src/Componentes/Tutorial.jsx
@@ -13,102 +13,102 @@ const Tutorial = () => {
 
     const tutorialSteps = [
         {
-            text: "¡Bienvenido a mi academia! Es mi objetivo que puedas aprender a invertir como un verdadero experto, ¡y hagas rendir esos ahorros!. Comencemos conociendo algunos conceptos básicos de las inversiones. ",
+            text: (<>¡Bienvenido a mi academia! Es mi objetivo que puedas aprender a invertir como un verdadero experto, ¡y hagas rendir esos ahorros!. Comencemos conociendo algunos conceptos básicos de las inversiones. </>),
         },
         {
-            text: "El objetivo de invertir es lograr que nuestro dinero incremente su valor. Existe un montón de motivos para querer invertir: incrementar nuestros ingresos, crear un fondo para retirarnos en un futuro; cualquier motivo es válido. Lo importante es tener en cuenta un principio básico: el dinero, si no se invierte, pierde valor..",
+            text: (<>El objetivo de invertir es lograr que nuestro dinero incremente su valor. Existe un montón de motivos para querer invertir: incrementar nuestros ingresos, crear un fondo para retirarnos en un futuro; cualquier motivo es válido. Lo importante es tener en cuenta un principio básico: el dinero, si no se invierte, <strong>pierde valor..</strong></>),
         },
         {
-            text: "Para evitar que nuestro dinero se desvalorice, entonces, debemos invertirlo. Hay muchísimas maneras de hacerlo, pero la amplia mayoría sigue un principio básico: usamos nuestro dinero para obtener un activo que incremente su valor en el tiempo, para luego venderlo y generar una ganancia.",
+            text: (<>Para evitar que nuestro dinero se desvalorice, entonces, debemos invertirlo. Hay muchísimas maneras de hacerlo, pero la amplia mayoría sigue un principio básico: usamos nuestro dinero para obtener un <strong>activo</strong> que incremente su valor en el tiempo, para luego venderlo y generar una ganancia.</>),
         },
         {
-            text: "En otras palabras, compramos un activo a un 'precio' determinado y lo vendemos a otro 'precio' más alto del que lo compramos, y esa diferencia de precio es nuestra ganancia, a la que también se llama renta. Al precio de un activo también se lo llama cotización.",
+            text: (<>En otras palabras, compramos un activo a un 'precio' determinado y lo vendemos a otro 'precio' más alto del que lo compramos, y esa diferencia de precio es nuestra <strong>ganancia</strong>, a la que también se llama <strong>renta</strong>. Al precio de un activo también se lo llama <strong>cotización</strong>.</>),
         },
         {
-            text: "Imagino que te estarás preguntando por qué los activos incrementan su valor en el tiempo. Esto depende del tipo de activo, y vas a aprender cómo funciona cada uno a lo largo de las distintas lecciones.",
+            text: (<>Imagino que te estarás preguntando por qué los activos incrementan su valor en el tiempo. Esto depende del tipo de activo, y vas a aprender cómo funciona cada uno a lo largo de las distintas lecciones.</>),
         },
         {
-            text: "Comencemos por el tipo de activo más básico: los activos de renta fija. El término 'renta fija' hace referencia a que vamos a saber de antemano cuánta ganancia (renta) vamos a obtener de la inversión. ",
+            text: (<>Comencemos por el tipo de activo más básico: los <strong>activos de renta fija</strong>. El término 'renta fija' hace referencia a que vamos a saber de antemano cuánta ganancia (renta) vamos a obtener de la inversión. </>),
         },
         {
-            text: "Este tipo de activo puede pensarse básicamente como un préstamo. Nosotros le prestamos nuestro dinero a una entidad por un tiempo determinado a cambio de que cuando nos lo devuelvan, lo hagan con un interés (es decir, que nos devuelvan más dinero del que les prestamos). Ese interés se pacta antes de que se realice la inversión, y no va a cambiar en el tiempo, por eso se dice que la renta es fija.",
+            text: (<>Este tipo de activo puede pensarse básicamente como un préstamo. Nosotros le prestamos nuestro dinero a una entidad por un tiempo determinado a cambio de que cuando nos lo devuelvan, lo hagan con un <strong>interés</strong> (es decir, que nos devuelvan más dinero del que les prestamos). Ese interés se pacta antes de que se realice la inversión, y no va a cambiar en el tiempo, por eso se dice que la renta es fija.</>),
         },
         {
-            text: "Ejemplos de este tipo de activo son los plazos fijos (le 'prestamos' al banco), cauciones (le 'prestamos' a una persona en el mercado), las obligaciones negociables (le 'prestamos' a una empresa) y los bonos (le 'prestamos al gobierno).",
+            text: (<>Ejemplos de este tipo de activo son los <strong>plazos fijos</strong> (le 'prestamos' al banco), <strong>cauciones</strong> (le 'prestamos' a una persona en el mercado), las <strong>obligaciones negociables</strong> (le 'prestamos' a una empresa) y los <strong>bonos</strong> (le 'prestamos al gobierno).</>),
         },
         {
-            text: "Los activos de renta fija son inversiones muy seguras, ya que siempre sabemos cuánto dinero vamos a ganar, y cuándo vamos a recibirlo. No hay volatilidad, es decir, no va a haber ningún cambio en nuestra renta ni en las condiciones del acuerdo. Es por este motivo por el cual son inversiones que generalmente dan ganancias pequeñas, y sirven más para cuidar el valor de nuestro dinero que para incrementar nuestro patrimonio.",
+            text: (<>Los activos de renta fija son inversiones muy seguras, ya que siempre sabemos cuánto dinero vamos a ganar, y cuándo vamos a recibirlo. No hay <strong>volatilidad</strong>, es decir, no va a haber ningún cambio en nuestra renta ni en las condiciones del acuerdo. Es por este motivo por el cual son inversiones que generalmente dan ganancias pequeñas, y sirven más para cuidar el valor de nuestro dinero que para incrementar nuestro <strong>patrimonio</strong> .</>),
         },
         {
-            text: "Como regla general, mientras más potencial de ganancia ofrezca una inversión, más alto es el riesgo que esta implica. Una buena regla general para las inversiones es tener en cuenta que si algo parece demasiado bueno para ser cierto, probablemente lo sea.",
+            text: (<>Como regla general, mientras <strong>más potencial de ganancia</strong> ofrezca una inversión, <strong>más alto es el riesgo</strong> que esta implica. Una buena regla general para las inversiones es tener en cuenta que si algo parece demasiado bueno para ser cierto, probablemente lo sea.</>),
         },
         {
-            text: "Comencemos haciendo tu primera inversión para terminar de entender estos conceptos.",
+            text: (<>Comencemos haciendo tu primera inversión para terminar de entender estos conceptos.</>),
             showImage: true,
             image: "/path/to/your/first/tutorial/image.jpg",
         },
         {
-            text: "Esta es la pantalla principal de nuestro broker. Tiene información clave sobre el estado de tu cuenta.",
+            text: (<>Esta es la pantalla principal de nuestro broker. Tiene información clave sobre el <strong>estado de tu cuenta</strong>.</>),
             showImage: true,
             image: "/path/to/your/first/tutorial/image.jpg",
         },
         {
-            text: "De momento se ve un poco vacía, pero va a ir completándose a medida que aprendamos más.",
+            text: (<>De momento se ve un poco vacía, pero va a ir completándose a medida que aprendamos más.</>),
             showImage: true,
             image: "/path/to/your/first/tutorial/image.jpg",
         },
         {
-            text: "Esto significa cada uno de los números que ves en pantalla.",
+            text: (<>Esto significa cada uno de los números que ves en pantalla.</>),
             showImage: true,
             image: "/path/to/your/first/tutorial/image.jpg",
         },
         {
-            text: "Vamos a hacer nuestra primera inversión entrando a la opción destacada en la barra superior.",
+            text: (<>Vamos a hacer nuestra primera inversión entrando a la opción destacada en la barra superior.</>),
             showImage: true,
             image: "/path/to/your/first/tutorial/image.jpg",
         },
         {
-            text: "Esta pantalla nos permite elegir entre las distintas opciones que tenemos para invertir.",
+            text: (<>Esta pantalla nos permite elegir entre las distintas opciones que tenemos para invertir.</>),
             showImage: true,
             image: "/path/to/your/first/tutorial/image.jpg",
         },
         {
-            text: "Seleccionando una, podemos acceder a detalles sobre ella. Seleccionemos “Plazo Fijo”.",
+            text: (<>Seleccionando una, podemos acceder a detalles sobre ella. Seleccionemos “Plazo Fijo”.</>),
             showImage: true,
             image: "/path/to/your/first/tutorial/image.jpg",
         },
         {
-            text: "En este caso, tenemos una descripción del tipo de activo. En otras lecciones tendremos descripciones más detalladas sobre el activo en sí, pero no nos adelantemos.",
+            text: (<>En este caso, tenemos una descripción del tipo de activo. En otras lecciones tendremos descripciones más detalladas sobre el activo en sí, pero no nos adelantemos.</>),
             showImage: true,
             image: "/path/to/your/first/tutorial/image.jpg",
         },
         {
-            text: "Esta tabla, que cambia dependiendo del tipo de activo, muestra la cotización actual, y nos indica si se modificó respecto de la última cotización. Esto va a tener más sentido luego.",
+            text: (<>Esta tabla, que cambia dependiendo del tipo de activo, muestra la cotización actual, y nos indica si se modificó respecto de la última cotización. Esto va a tener más sentido luego.</>),
             showImage: true,
             image: "/path/to/your/first/tutorial/image.jpg",
         },
         {
-            text: "Este gráfico muestra la evolución de la cotización del activo a lo largo del tiempo. Es útil para poder tener una idea general de cómo se comporta. Es muy útil para activos volátiles.",
+            text: (<>Este gráfico muestra la evolución de la cotización del activo a lo largo del tiempo. Es útil para poder tener una idea general de cómo se comporta. Es muy útil para activos <strong>volátiles</strong>.</>),
             showImage: true,
             image: "/path/to/your/first/tutorial/image.jpg",
         },
         {
-            text: "Esta tabla nos muestra nuestras tenencias, es decir, cuánto tenemos invertido en este activo. Nos muestra cuántas unidades tenemos, si aplica, y su cotización actual.",
+            text: (<>Esta tabla nos muestra nuestras <strong>tenencias</strong>, es decir, cuánto tenemos invertido en este <strong>activo</strong>. Nos muestra cuántas unidades tenemos, si aplica, y su <strong>cotización</strong> actual.</>),
             showImage: true,
             image: "/path/to/your/first/tutorial/image.jpg",
         },
         {
-            text: "En este apartado tenemos características generales del activo, que nos pueden ayudar a decidir si nos conviene invertir.",
+            text: (<>En este apartado tenemos características generales del <strong>activo</strong>, que nos pueden ayudar a decidir si nos conviene invertir.</>),
             showImage: true,
             image: "/path/to/your/first/tutorial/image.jpg",
         },
         {
-            text: "Finalmente, tenemos los botones de compra y venta del activo. Para plazo fijo en particular, no aplica la venta y por eso lo vemos oscurecido.",
+            text: (<>Finalmente, tenemos los botones de compra y venta del activo. Para <strong>plazo fijo</strong> en particular, no aplica la venta y por eso lo vemos oscurecido.</>),
             showImage: true,
             image: "/path/to/your/first/tutorial/image.jpg",
         },
         {
-            text: "Hagamos clic en Comprar para hacer nuestra primera inversión.",
+            text: (<>Hagamos clic en <strong>Comprar</strong> para hacer nuestra primera inversión.</>),
             showImage: true,
             image: "/path/to/your/first/tutorial/image.jpg",
         },
